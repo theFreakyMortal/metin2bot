23441934ca465f50c2dae1235607c716ba6f7161
Arda <ardayldzyldzarda@gmail.com>
2025-01-20 10:08:21 +0300

    a

---
+++ main.py
@@ -0,0 +1,71 @@
+import cv2, time, numpy as np, pyautogui, pynput
+from pynput.keyboard import Controller, Key
+import os
+
+cap = cv2.VideoCapture(0)
+cap.release()
+
+keyboard = Controller()
+
+def thresholding(img):
+    if img is not None:
+        imgGray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        _, holded = cv2.threshold(imgGray, 245, 255, cv2.THRESH_BINARY)
+        print('Thresholded')
+        return holded.astype(np.uint8)
+
+def screen_recorder():
+    img = np.array(pyautogui.screenshot(region=(0, 0, 1920, 1080)))
+    print('Screen recorded')
+    return img
+
+def on_press(key):
+    if key == Key.esc:
+        print("Exiting program...")
+        return False
+
+def main(template_path):
+    if not os.path.exists(template_path):
+        print(f"Template image not found at {template_path}.")
+        return
+
+    template = cv2.imread(template_path, cv2.IMREAD_GRAYSCALE)
+    if template is None:
+        print("Template image not found.")
+        return
+
+    while True:
+        img = screen_recorder()
+        holded = thresholding(img)
+        
+        if holded.shape[0] < template.shape[0] or holded.shape[1] < template.shape[1]:
+            print("Template size is larger than the image.")
+            continue
+
+        res = cv2.matchTemplate(holded, template, cv2.TM_CCOEFF_NORMED)
+        loc = np.where(res >= 0.9)
+        for pt in zip(*loc[::-1]):
+            screen_center = (holded.shape[1] // 2, holded.shape[0] // 2)
+            closest_pt = min(zip(*loc[::-1]), key=lambda pt: (pt[0] - screen_center[0]) ** 2 + (pt[1] - screen_center[1]) ** 2)
+        pynput.mouse.Controller().position = (int(closest_pt[0])+template.shape[1]//2, int(closest_pt[1])+30)
+        time.sleep(.1)
+        pynput.mouse.Controller().click(pynput.mouse.Button.left, count=1)
+        time.sleep(.1)
+        pynput.keyboard.Controller().press(Key.alt)
+        time.sleep(.1)
+        pynput.keyboard.Controller().press(Key.tab)
+        time.sleep(.1)
+        pynput.keyboard.Controller().release(Key.tab)
+        time.sleep(.1)
+        pynput.keyboard.Controller().press(Key.tab)
+        time.sleep(.1)
+        pynput.keyboard.Controller().release(Key.tab)
+        time.sleep(.1)
+        pynput.keyboard.Controller().release(Key.alt)
+        time.sleep(1)
+        
+
+if __name__ == "__main__":
+    template_path = "stones\\" + input('Stone: ') + ".png"
+    time.sleep(10)
+    main(template_path)
---
+++ teplater.py
@@ -0,0 +1,27 @@
+import cv2 as cv, pyautogui, numpy as np, pynput, time
+
+keyboard_controller = pynput.keyboard.Controller()
+
+stoneName = input('Enter the name of the stone: ')
+
+locates = []
+
+def teplater(key):
+    try:
+        if key.char == 'p':
+            locates.append(pyautogui.position())
+            if len(locates) == 2:
+                x1, y1 = locates[0]
+                x2, y2 = locates[1]
+                img = pyautogui.screenshot(region=(x1, y1, x2 - x1, y2 - y1))
+                img = np.array(img)
+                img = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
+                img = cv.threshold(img, 245, 255, cv.THRESH_BINARY)
+                cv.imwrite(f'stones/{stoneName}.png', img[1])
+                print('Stone saved')
+                return 
+    except AttributeError:
+        pass
+
+with pynput.keyboard.Listener(on_press=teplater) as listener:
+    listener.join()
